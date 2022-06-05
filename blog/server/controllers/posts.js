37c8945537e421132ed7470ff40a51d45d8e26a5
İslam Uyanık <90751829+uyanikislam@users.noreply.github.com>
2022-06-05 23:37:49 +0300

    Initial commit

---
+++ blog/server/controllers/posts.js
@@ -0,0 +1,12 @@
+import Post from "../models/posts.js";
+
+export const getPosts = async (req, res) => {
+    try{
+        const posts= await Post.find();
+        res.status(200).json(posts);
+      } catch (error) {
+        res.status(404).json ({
+            message: error.message,
+        });
+    }
+};
